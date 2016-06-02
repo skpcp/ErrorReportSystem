d77423ce31d00e934f17c6c7ac34f1f7031926cc
BidziS <danielcudnik93@gmail.com>
2016-06-02 21:46:12 +0200

    commit

--- app/src/main/java/com/example/bidzis/errorreportsystem/LoginActivity.java
+++ app/src/main/java/com/example/bidzis/errorreportsystem/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.bidzis.errorreportsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText etLogin = (EditText) findViewById(R.id.etLogin);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final Button btSignIn = (Button) findViewById(R.id.btSignin);
     }
 }
